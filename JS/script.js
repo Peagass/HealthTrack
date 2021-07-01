3e970112b6a70798e7310622780bba2a8dc04d3f
Paulo Gomes <paulo1992_27@hotmail.com>
2021-07-01 17:00:28 -0300

    Primeiro Commit

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function sucesso() {
+    alert("Seus dados foram atualizados com Sucesso.");
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
